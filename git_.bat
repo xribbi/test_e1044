c5d7b8c40ac5d2bb18d4334344395d023d3b01b4
xribbi <xribbi.oz@gmail.com>
2024-11-12 15:54:14 +0900

    _

--- git_.bat
+++ git_.bat
@@ -2,17 +2,16 @@
 :
 : for minor/incremental git update(s) 
 :
-
 @echo on
-@rem ********************************************************************************
-@rem ** Fetch change(s) from the remote and merge current branch with its upstream **
-@rem ********************************************************************************
+
+rem ********************************************************************************
+rem ** Fetch change(s) from the remote and merge current branch with its upstream **
+rem ********************************************************************************
 git pull
 
-@echo on
-@rem ********************************************************************************
-@rem ** Push the local branch to the remote repository. Set its copy as an upstream**
-@rem ********************************************************************************
+rem ********************************************************************************
+rem ** Push the local branch to the remote repository. Set its copy as an upstream**
+rem ********************************************************************************
 git add *
 git commit -m "_"
 git push
