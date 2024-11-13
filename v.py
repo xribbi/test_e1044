e43a9c7fbcee6d1bb93ad58b38b07c9d51ca99c2
xribbi <xribbi.oz@gmail.com>
2024-11-14 00:57:42 +0900

    _

--- v.py
+++ v.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
-def v(e): print(e+" ==> "+str(eval(e))+"\n");  
+def a(o): [print("%04d:%20s:%s"%(i,a,getattr(o,a)) for (i,a) in enumerate(dir(o),1))]  
+def v(e): print(e+" ==> "+str(eval(e)));
 
+print("_______")
 v("os.getcwd()")
 v("sys.version")
